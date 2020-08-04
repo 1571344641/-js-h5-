8d0c91eb9dda42f57e2300da7ccf43bc6b731543
张志岳 <3334913026@qq.com>
2020-08-05 01:04:09 +0800

    左侧list选择切换卡片完成

--- assecte/js/index.js
+++ assecte/js/index.js
@@ -1,11 +1,30 @@
 //left-aside 的li切换
 let active = 0;//保存上一次被点击的下标
+let cards = 0;
 const list = document.querySelectorAll('.self-info > ul > li')
+//获取右边的四张卡片(项目经验....)
+const card = document.querySelectorAll('.self-card')
+//遮罩层
+const shadle = document.querySelector('.shadle')
+//点击左边的列表改变右边的卡片
 list.forEach((item,index) => {
     item.addEventListener('click',() => {
+        //切换小三角
         list[active].classList.toggle('active')
         active = index;
         list[active].classList.toggle('active')
+        //切换小三角完
+        //card切换和遮罩层定时器
+        shadle.style.display = 'block'
+        setTimeout(() =>{
+            shadle.style.display = 'none'
+        },500)
+        setTimeout(() =>{
+            card[cards].style.display = "none"
+            cards = index;
+            card[cards].style.display = "flex"
+
+        },300)
     })
 });
 //点击显示个人经历动画
