16fea431c9268291566ae845edd9975fe4fba223
张志岳 <3334913026@qq.com>
2020-08-04 19:08:29 +0800

    内容区域开发完成

--- assecte/js/index.js
+++ assecte/js/index.js
@@ -8,3 +8,26 @@ list.forEach((item,index) => {
         list[active].classList.toggle('active')
     })
 });
+//点击显示个人经历动画
+const next = document.querySelector('.next')
+const back = document.querySelector('.back')
+next.onclick = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    next.parentElement.classList.remove('self_b')
+    back.parentElement.classList.remove('disappear')
+    next.parentElement.classList.toggle('self_a')
+    back.parentElement.classList.toggle('appear')
+}
+back.onclick = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    back.parentElement.classList.remove('appear')
+    back.parentElement.classList.toggle('disappear')
+    next.parentElement.classList.toggle('self_b')
+    next.parentElement.classList.remove('self_a')
+
+
+
+
+}
